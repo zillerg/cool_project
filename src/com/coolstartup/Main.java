7318fbcb1e53ae25bf6d9048ae243ddd89b867b4
zalkar <zalkar.ai7@gmail.com>
2020-09-17 13:12:44 -0400

    new method

--- src/com/coolstartup/Main.java
+++ src/com/coolstartup/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello, I'm cool project ");
+    }
+
+    public static  int sumOfTwo(int a, int b){
+        return a+b;
     }
 }
